c697dc211ab2cb264ef40fba8d5c330251ca169e
Rener-Pontes <renercpontes@gmail.com>
2023-02-22 23:13:13 -0300

    Twelveth tutorial video. Create all the roll animations, create a roll blendspace, implement the roll state.

--- Player/Scripts/Player.gd
+++ Player/Scripts/Player.gd
@@ -2,9 +2,10 @@ extends KinematicBody2D
 
 
 # Declare member constants here.
-const MAX_SPEED = 100
+const MAX_SPEED = 80
 const ACCELARATION = 500 
 const FRICTION = 500
+const ROLL_SPEED = 120
 
 
 enum {
@@ -16,6 +17,7 @@ enum {
 
 # Declare member variables here. Examples:
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.DOWN
 var state = MOVE
 
 onready var animationPlayer = $AnimationPlayer
@@ -34,7 +36,7 @@ func _process(delta):
 		MOVE:
 			move_state(delta)
 		ROLL:
-			pass
+			roll_state(delta)
 		ATTACK:
 			attack_state(delta)
 
@@ -49,9 +51,12 @@ func move_state(delta):
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELARATION * delta)
 		
+		roll_vector = input_vector
+		
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		
 		animationState.travel("Run")
 	else:
@@ -59,14 +64,34 @@ func move_state(delta):
 		
 		animationState.travel("Idle")
 
-	velocity = move_and_slide(velocity)
+	move()
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
+
+
+func move():
+	velocity = move_and_slide(velocity)
+
 
 func attack_state(delta):
 	animationState.travel("Attack")
 
+
+func roll_state(delta):
+	velocity = roll_vector * ROLL_SPEED
+	move()
+	
+	animationState.travel("Roll")
+
+
 func attack_animation_finished():
 	velocity = Vector2.ZERO
+	state = MOVE
+
+
+func roll_animation_finished():
+	velocity *= .8
 	state = MOVE

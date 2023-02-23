e1cb580558fc3c17c85411bcdcf6527572863462
Rener-Pontes <renercpontes@gmail.com>
2023-02-22 22:01:51 -0300

    Tenth tutorial video. Create the Grass and its effect Scene, implemation of instancing the effect via code.

---
+++ World/Scripts/Grass.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("attack"):
+		var grassEffect = load("res://Effects/Scenes/GrassEffect.tscn")
+		var grassEffectInstance = grassEffect.instance()
+		var world = get_tree().current_scene  
+		
+		world.add_child(grassEffectInstance)
+		grassEffectInstance.global_position = global_position
+		
+		queue_free()
